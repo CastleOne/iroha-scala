8897651ce6128f7a7f727d9d20940ff1e041cdaa
bagf <rory@eishgaming.co.za>
2019-02-26 01:53:46 +0200

    Correct namespace and copy over hash constructor for SHA3EdDSAPrivateKey
    Signed-off-by: bagf <rory@eishgaming.co.za>

--- src/test/scala/net/cimadai/crypto/CryptoSpec.scala
+++ src/test/scala/net/cimadai/crypto/CryptoSpec.scala
@@ -1,4 +1,4 @@
-package net.cimadai.iroha.crypto
+package net.cimadai.crypto
 
 import utest._
 
@@ -10,6 +10,7 @@ object CryptoSpec extends TestSuite {
     "SHA3EdDSAKeyPair should match a known, valid, existing key pair"-{
       val privateKey = "f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70"
       val publicKey  = "313a07e6384776ed95447710d15e59148473ccfc052a681317a72a69f2a49910"
+
       SHA3EdDSAKeyPair(privateKey) match {
         case Success(keypair) =>
           val actual = keypair.publicKey.toPublicKeyHex
